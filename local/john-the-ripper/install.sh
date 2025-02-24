58e552aa1f092c923d3eafdb80c90fd20bb217c2
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2025-02-24 11:06:34 +0000

    local/john-the-ripper: Fix download link

--- local/john-the-ripper/install.sh
+++ local/john-the-ripper/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-unzip -o john-c7cacb14f5ed20aca56a52f1ac0cd4d5035084b6.zip
+unzip -o c7cacb14f5ed20aca56a52f1ac0cd4d5035084b6.zip
 cd john-c7cacb14f5ed20aca56a52f1ac0cd4d5035084b6/src/
 ./configure --disable-native-tests --disable-opencl
 make -j $NUM_CPU_CORES
