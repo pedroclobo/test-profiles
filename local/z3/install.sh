1ce6b683c02557bf2e6ebe55be258c901adf4641
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2025-03-05 12:41:40 +0000

    local/z3: Specify compiler flags

--- local/z3/install.sh
+++ local/z3/install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 tar -xvf z3_solver-4.14.0.0.tar.gz
 pushd z3_solver-4.14.0.0/core
-CC=$CC CXX=$CXX python scripts/mk_make.py
-python3 scripts/mk_make.py
+CC=$CC CXX=$CXX CFLAGS=$CFLAGS CXXFLAGS=$CXXFLAGS python3 scripts/mk_make.py
 cd build
 make -j$NUM_CPU_CORES
 popd
