c611243d52f92b5abaaf71849b993b3649f0a149
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2025-03-29 17:36:30 +0000

    local/openssl: Change test duration to 15 seconds

--- local/openssl/install.sh
+++ local/openssl/install.sh
@@ -7,6 +7,6 @@ echo $? > ~/install-exit-status
 cd ~
 echo "#!/bin/sh
 cd openssl-3.3.0
-LD_LIBRARY_PATH=.:\$LD_LIBRARY_PATH $PIN_CMD ./apps/openssl speed -multi \$NUM_CPU_CORES -seconds 30 \$@ > \$LOG_FILE 2>&1
+LD_LIBRARY_PATH=.:\$LD_LIBRARY_PATH $PIN_CMD ./apps/openssl speed -multi \$NUM_CPU_CORES -seconds 15 \$@ > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > openssl
 chmod +x openssl
