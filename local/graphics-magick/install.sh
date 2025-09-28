d24a9a73b051db1b01685f79e969bdb84580258f
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2025-09-28 17:26:40 +0100

    local/graphics-magick: Don't delete object files

--- local/graphics-magick/install.sh
+++ local/graphics-magick/install.sh
@@ -7,9 +7,6 @@ make -j $NUM_CPU_CORES
 echo $? > ~/install-exit-status
 make install
 cd ~
-rm -rf GraphicsMagick-1.3.43/
-rm -rf gm_/share/doc/GraphicsMagick/
-rm -rf gm_/share/man/
 ./gm_/bin/gm convert sample-photo-mars.jpg input.mpc
 echo "#!/bin/sh
 OMP_NUM_THREADS=\$NUM_CPU_CORES $PIN_CMD ./gm_/bin/gm benchmark -duration 60 convert input.mpc \$@ null: > \$LOG_FILE 2>&1
