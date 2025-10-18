b7c5437f1384512450d83ab562c946c53f6981bd
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2025-10-18 13:14:31 +0100

    local/simdjson: add test

---
+++ local/simdjson/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+rm -rf simdjson-2.0.4
+tar -xf simdjson-2.0.4.tar.gz
+cd simdjson-2.0.4
+sed -i -E 's/operator[[:space:]]+""[[:space:]]+_/operator""_/g' singleheader/simdjson.h
+sed -i -E 's/operator[[:space:]]+""[[:space:]]+_/operator""_/g' include/simdjson/padded_string-inl.h
+sed -i -E 's/operator[[:space:]]+""[[:space:]]+_/operator""_/g' include/simdjson/padded_string.h
+sed -i '734i (void) count;' tests/dom/document_stream_tests.cpp
+mkdir build
+cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DSIMDJSON_JUST_LIBRARY=OFF
+make -j $NUM_CPU_CORES
+echo $? > ~/install-exit-status
+cd ~
+
+echo "#!/bin/sh
+cd simdjson-2.0.4/build/benchmark
+./bench_ondemand --benchmark_min_time=30 --benchmark_filter=\$@\<simdjson_ondemand\> > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > simdjson
+chmod +x simdjson
