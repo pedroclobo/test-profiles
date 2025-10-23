053e7af2e22fe68cc90714899986bc2e9ec69c81
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2025-10-23 11:58:40 +0100

    local/build-llvm: install script compiles llvm

--- local/build-llvm/install.sh
+++ local/build-llvm/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-echo "#!/bin/sh
+
+mkdir build
+tar -xf llvm-16.0.0.src.tar.xz
+tar -xf cmake-16.0.0.src.tar.xz
+mv cmake-16.0.0.src cmake
 cd build
-cmake --build . -- -j \$NUM_CPU_CORES 2>&1
-echo \$? > ~/test-exit-status" > build-llvm
-chmod +x build-llvm
+cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_TESTS=OFF -DLLVM_INCLUDE_TESTS=OFF -G Ninja ../llvm-16.0.0.src
+cmake --build . -- -j $NUM_CPU_CORES
+
